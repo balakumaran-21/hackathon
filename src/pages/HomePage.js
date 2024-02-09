1cca37f408e972e85331139a7bdaf879a7085f1d
Balakumaran <balakumaran.ma@prodapt.com>
2024-02-09 23:47:40 +0530

    homepage layout

---
+++ src/pages/HomePage.js
@@ -0,0 +1,35 @@
+const HomePage = () => {
+  return (
+    <div className="home-div">
+      <div className="left">
+        <div className="text-div">
+          <h1>Internet</h1>
+          <button className="details-btn"> Details</button>
+        </div>
+        <div className="image"></div>
+      </div>
+      <div className="right">
+        <div className="text-div">
+          <h1>OTL</h1>
+          <button className="details-btn"> Details</button>
+        </div>
+        <div className="image"></div>
+      </div>
+      <div className="left">
+        <div className="text-div">
+          <h1>Internet</h1>
+          <button className="details-btn"> Details</button>
+        </div>
+        <div className="image"></div>
+      </div>
+      <div className="right">
+        <div className="text-div">
+          <h1>OTL</h1>
+          <button className="details-btn"> Details</button>
+        </div>
+        <div className="image"></div>
+      </div>
+    </div>
+  );
+}
+export default HomePage
